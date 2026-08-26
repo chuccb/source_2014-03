26c5d21b392fa77b30fcfa17a84be17cab8672e6
chuccb <tjo6tjo6@outlook.com>
2026-08-27 07:43:49 +0800

    Register gup_delete_unit schema migration

--- KncWX2Server.Net/src/KncWX2Server.Persistence/MigrationManifest.cs
+++ KncWX2Server.Net/src/KncWX2Server.Persistence/MigrationManifest.cs
@@ -9,6 +9,7 @@ internal static class MigrationManifest
         new("001_initial", "KncWX2Server.Persistence.Migrations.001_initial.sql"),
         new("002_gup_create_unit_schema", "KncWX2Server.Persistence.Migrations.002_gup_create_unit_schema.sql"),
         new("003_fix_gunit_nickname_schema", "KncWX2Server.Persistence.Migrations.003_fix_gunit_nickname_schema.sql"),
+        new("004_gup_delete_unit_schema", "KncWX2Server.Persistence.Migrations.004_gup_delete_unit_schema.sql"),
     ];
 
     public static ReadOnlySpan<MigrationDefinition> All => s_all;
