411976259558eb76e33aab435863c700bb62267c
chuccb <tjo6tjo6@outlook.com>
2026-08-26 08:45:02 +0800

    Move verified Common exception primitives into Core

---
+++ KncWX2Server.Net/src/KncWX2Server.Core/Common/KncException.cs
@@ -0,0 +1,23 @@
+namespace KncWX2Server.Core.Common;
+
+public class KncException(string message, Exception? innerException = null)
+    : Exception(message, innerException);
+
+public static class KncExceptionFormatter
+{
+    public static string Format(Exception exception, string? context = null)
+    {
+        ArgumentNullException.ThrowIfNull(exception);
+
+        var timestamp = DateTimeOffset.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture);
+        var location = string.IsNullOrWhiteSpace(context) ? string.Empty : $", {context}";
+        return $"{timestamp}{location}, {exception.GetType().Name}: {exception.Message}";
+    }
+
+    public static string FormatUnknown(string? context = null)
+    {
+        var timestamp = DateTimeOffset.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture);
+        var location = string.IsNullOrWhiteSpace(context) ? string.Empty : $", {context}";
+        return $"{timestamp}{location}, Unknown Exception Occured.";
+    }
+}
