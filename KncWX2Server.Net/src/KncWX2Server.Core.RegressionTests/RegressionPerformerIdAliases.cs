50f38199307daee105a7f87d0d1e473303d715f7
chuccb <tjo6tjo6@outlook.com>
2026-08-28 00:27:55 +0800

    Fix regression performer ID references

---
+++ KncWX2Server.Net/src/KncWX2Server.Core.RegressionTests/RegressionPerformerIdAliases.cs
@@ -0,0 +1,10 @@
+using KncWX2Server.Core.Common.Routing;
+
+global using PerformerIds = RegressionPerformerIdAliases;
+
+internal static class RegressionPerformerIdAliases
+{
+    public const uint PiGsServer = (uint)PerformerId.GsServer;
+    public const uint PiGsUser = (uint)PerformerId.GsUser;
+    public const uint PiGsGameDb2 = (uint)PerformerId.GsGameDb2nd;
+}
